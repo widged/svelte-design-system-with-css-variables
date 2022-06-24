2933ce193eff828408bdfcd6a4aed54e7e6d3c0d
inclusive-ml <code@inclusive-ml.com>
2022-06-24 13:36:59 +0200

    feat: toggle

--- src/packages/base/toggle/README.md
+++ src/packages/base/toggle/README.md
@@ -1,3 +1,5 @@
+# Toggle
+
 ```
 yarn parcel __usage__/index.html
 ```
--- src/packages/base/toggle/__usage__/Scenario.svelte
+++ src/packages/base/toggle/__usage__/Scenario.svelte
@@ -2,13 +2,34 @@
   import Toggle from '../';
 </script>
 
-<h5>✦ default</h5>
-<Toggle />
-<h5>✦ labels defined</h5>
-<Toggle labels={['on', 'off']} />
-<h5>✦ labels are in the wrong format</h5>
-<Toggle labels="wrong format" />
-<h5>✦ disabled</h5>
-<Toggle disabled={true} />
-<h5>✦ style overwrite</h5>
-<Toggle style="--color-bg: red" />
+<section>
+  <article>
+    <h5>✦ default</h5>
+    <Toggle />
+    <h5>✦ checked</h5>
+    <Toggle checked={true} />
+  </article>
+  <article>
+    <h5>✦ disabled</h5>
+    <Toggle disabled={true} />
+    <h5>✦ style overwrite</h5>
+    <Toggle style="--color-bg: #333333" />
+  </article>
+  <article>
+    <h5>✦ labels are in the wrong format</h5>
+    <Toggle labels="wrong format" />
+    <h5>✦ labels in the correct format</h5>
+    <Toggle labels={['on', 'off']} />
+  </article>
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: row;
+    gap: 30px;
+    justify-content: space-around;
+  }
+  article {
+  }
+</style>
