2467a0aa364a1431acbcc555568a0e56339648d3
inclusive-ml <code@inclusive-ml.com>
2022-06-24 15:28:58 +0200

    feat: tooltip

---
+++ src/packages/base/tooltip/README.md
@@ -0,0 +1,10 @@
+# Tooltip
+
+```
+yarn init
+yarn parcel __usage__/index.html
+```
+
+## Credits
+
+Derived from https://github.com/kazzkiq/balloon.css
---
+++ src/packages/base/tooltip/__usage__/Scenario.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Badge from '../';
+</script>
+
+<div class="px-8">
+  <h1>Scenario -- label defined</h1>
+  <Badge label="10" />
+</div>
+
+<style>
+  :root {
+    --size-1: 4px;
+    --pad-sm: 10px;
+    --color-text: white;
+    --color-bg-secondary: #2c7a7b;
+    --radius-full: 9999px;
+  }
+
+  h1 {
+    font-size: 14px;
+    color: blue;
+  }
+</style>
